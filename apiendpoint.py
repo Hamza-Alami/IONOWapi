886ca334207c2b85fa92cac102369ea3f52a0bb8
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-03-29 22:22:42 +0000

    Update apiendpoint.py

--- apiendpoint.py
+++ apiendpoint.py
@@ -1,44 +1,41 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import datetime
-from datetime import datetime as dt
-from datetime import timedelta
-import os, sys
-import base64
-import requests
-import io
-from io import BytesIO
 import json
-from datetime import date
-import time
+import requests
 
-base_urls = [
-    'https://valead.bitrix24.com/rest/2593/1qypbfjokvl3q7n9/crm.deal.list.json?Filter[STAGE_ID]=C51:WON&',
-    'https://valead.bitrix24.com/rest/2593/0yy34uz3ome8hk4o/crm.deal.list.json?Filter[STAGE_ID]=C51:NEW&',
-    'https://valead.bitrix24.com/rest/2593/rg46gyrl6kfedteu/crm.deal.list.json?Filter[STAGE_ID]=C51:LOSE&',
-    'https://valead.bitrix24.com/rest/2593/tcdo9l8bdswx5t2t/crm.deal.list.json?Filter[STAGE_ID]=C51:1&',
-    'https://valead.bitrix24.com/rest/2593/2c0lhsi6b0biyo44/crm.deal.list.json?Filter[STAGE_ID]=C51:EXECUTING&',
-    'https://valead.bitrix24.com/rest/2593/vjpem3zwd2k5tzff/crm.deal.list.json?Filter[STAGE_ID]=C51:FINAL_INVOICE&',
-    'https://valead.bitrix24.com/rest/2593/tjp12j1b4wp8bap5/crm.deal.list.json?Filter[STAGE_ID]=C51:PREPARATION&'
-]
+from flask import Flask, jsonify
 
-data = []
-for url in base_urls:
-    start = 0
-    count = 50
+app = Flask(__name__)
 
-    # Retrieve the total number of records
-    response = requests.get(f"{url}&start={start}&select[]=ID&count=1")
-    total_records = response.json()['total']
+@app.route('/get_deal_data')
+def get_deal_data():
+    base_urls = [
+        'https://valead.bitrix24.com/rest/2593/1qypbfjokvl3q7n9/crm.deal.list.json?Filter[STAGE_ID]=C51:WON&',
+        'https://valead.bitrix24.com/rest/2593/0yy34uz3ome8hk4o/crm.deal.list.json?Filter[STAGE_ID]=C51:NEW&',
+        'https://valead.bitrix24.com/rest/2593/rg46gyrl6kfedteu/crm.deal.list.json?Filter[STAGE_ID]=C51:LOSE&',
+        'https://valead.bitrix24.com/rest/2593/tcdo9l8bdswx5t2t/crm.deal.list.json?Filter[STAGE_ID]=C51:1&',
+        'https://valead.bitrix24.com/rest/2593/2c0lhsi6b0biyo44/crm.deal.list.json?Filter[STAGE_ID]=C51:EXECUTING&',
+        'https://valead.bitrix24.com/rest/2593/vjpem3zwd2k5tzff/crm.deal.list.json?Filter[STAGE_ID]=C51:FINAL_INVOICE&',
+        'https://valead.bitrix24.com/rest/2593/tjp12j1b4wp8bap5/crm.deal.list.json?Filter[STAGE_ID]=C51:PREPARATION&'
+    ]
 
-    while start < total_records:
-        url_with_params = f"{url}&start={start}"
-        response = requests.get(url_with_params)
-        response_json = response.json()
+    data = []
+    for url in base_urls:
+        start = 0
+        count = 50
 
-        data.extend(response_json['result'])
-        start += 50  # update start parameter to retrieve next 50 records
+        # Retrieve the total number of records
+        response = requests.get(f"{url}&start={start}&select[]=ID&count=1")
+        total_records = response.json()['total']
 
+        while start < total_records:
+            url_with_params = f"{url}&start={start}"
+            response = requests.get(url_with_params)
+            response_json = response.json()
 
-st.write(f"Retrieved {len(data)} records")
+            data.extend(response_json['result'])
+            start += 50  # update start parameter to retrieve next 50 records
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
