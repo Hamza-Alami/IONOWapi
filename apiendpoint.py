e469eeffdaa8e2549ab2b473de51f9fd74e3dc6c
Hamza ALAMI <80399028+Hamza-Alami@users.noreply.github.com>
2023-03-30 20:37:18 +0000

    Update apiendpoint.py

--- apiendpoint.py
+++ apiendpoint.py
@@ -1,6 +1,6 @@
 import requests
-import json
 import streamlit as st
+from streamlit import share
 
 base_urls = [
     'https://valead.bitrix24.com/rest/2593/1qypbfjokvl3q7n9/crm.deal.list.json?Filter[STAGE_ID]=C51:WON&',
@@ -29,24 +29,7 @@ for url in base_urls:
         data.extend(response_json['result'])
         start += 50  # update start parameter to retrieve next 50 records
 
-# convert data to JSON format
-json_data = json.dumps(data)
+st.write(data)
 
-# get the URL of the Streamlit app
-streamlit_url = st.get_share_streamlit_url()
-
-# print the URL on the Streamlit page
-st.write("Use this URL to access the data in Power BI:")
-st.write(streamlit_url)
-
-# serve the data as a JSON file
-response = requests.Response()
-response.headers['Content-Type'] = 'application/json'
-response.headers['Content-Disposition'] = 'attachment; filename=data.json'
-response.content = json_data.encode('utf-8')
-
-# return the response
-st.write("Here is a sample of the data:")
-st.json(data[:10])
-st.write("To download the full data set, right-click the link below and select 'Save link as':")
-st.markdown(f'<a href="data:application/json;base64,{response.content.decode("utf-8")}" download="data.json">Download data as JSON file</a>', unsafe_allow_html=True)
+share_url = share.get_public_share_url()
+st.write(f"Use this URL to access the data: {share_url}")
